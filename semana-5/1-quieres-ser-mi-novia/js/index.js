48a33e828faeea32c2fd4b5d444103889a05b7ea
Drocu <drocu99@gmail.com>
2022-08-29 18:38:26 -0500

    s5: constantes en JavaScript

---
+++ semana-5/1-quieres-ser-mi-novia/js/index.js
@@ -0,0 +1,11 @@
+// alert('Hola mundo');
+// let rodrigo = 26;
+// console.log(rodrigo);
+// let edad = 16;
+// console.log('¿Es mayor de edad?');
+// console.log((edad > 17)? 'Eres mayor de edad' : 'Eres menor de edad');
+
+const PI =3.141516;
+console.log(PI);
+const DAYS_WEEK = 7;
+console.log(DAYS_WEEK);
