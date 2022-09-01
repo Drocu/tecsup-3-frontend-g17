27db2479f232e8d9c3ea599d00b2ee446f5c9308
Drocu <drocu99@gmail.com>
2022-08-31 23:11:47 -0500

    s5: java script funcionalidad a la pagina

--- semana-5/1-quieres-ser-mi-novia/js/index.js
+++ semana-5/1-quieres-ser-mi-novia/js/index.js
@@ -1,145 +1,259 @@
 // alert('Hola mundo');
-// let rodrigo = 26;
-// console.log(rodrigo);
-// let edad = 16;
+
+// console.log(undefined);
+// console.log(true);
+// console.log(26);
+// console.log('Elliot');
+
+// let elliot;
+// console.log(elliot);
+// elliot = 26;
+// console.log(elliot);
+// elliot = 'Peruano';
+// console.log(elliot);
+// elliot = false;
+// console.log(elliot);
+
+// const nombre = 'Elliot';
+// console.log(nombre);
+
+// console.log(2 == 2);
+// console.log(2 == '2');
+// console.log(2 != 2);
+// console.log(2 != '2');
+// console.log(2 === 2);
+// console.log(2 === '2');
+// console.log(2 !== '2');
+// console.log(18 < 18);
+// console.log(18 <= 18);
+// console.log(17 < 18);
+// console.log(17 <= 18);
+// console.log(18 > 18);
+// console.log(18 >= 18);
+// console.log(17 > 18);
+// console.log(17 >= 18);
+
+// console.log(8 + 5);
+// console.log(5 - 8);
+// console.log(5 * 8);
+// console.log(8 / 5);
+// console.log(8 ** 2);
+// console.log(8 % 5);
+
+// let edad = 26;
+// console.log(edad);
+// console.log(edad++);
+// console.log(edad);
+// console.log(++edad);
+// console.log(edad);
+
+// console.log(edad);
+// console.log(edad--);
+// console.log(edad);
+// console.log(--edad);
+// console.log(edad);
+
+// console.log('5');
+// console.log(+'5');
+// console.log(-'5');
+
+// console.log(true && 'Elliot');
+// console.log(false && 'Elliot');
+
+// console.log(true || 'Elliot');
+// console.log(false || 'Elliot');
+
+// console.log(!true);
+
+// console.log('Elliot' + ' Leo');
+// console.log(8 + 8  + ' Leo');
+// console.log('8' + 8  + ' Leo');
+// let nombres = 'Elliot';
+// // nombres = nombres + ' Leo';
+// nombres = nombres + ' Leo';
+// console.log(nombres)
+
+// let edad = 26;
 // console.log('¿Es mayor de edad?');
-// console.log((edad > 17)? 'Eres mayor de edad' : 'Eres menor de edad');
+// console.log((edad > 17) ? 'Eres mayor de edad' : 'Eres menor de edad');
 
-// const PI =3.141516;
+// let comidaEstofado = 'Chancho';
+// console.log(comidaEstofado);
+// comidaEstofado = 'Pollo';
+// console.log(comidaEstofado);
+
+// const PI = 3.14;
 // console.log(PI);
-// const DAYS_WEEK = 7;
-// console.log(DAYS_WEEK);
+// const DAYS_OF_THE_WEEK = 7;
+// console.log(DAYS_OF_THE_WEEK);
 
-// let nombre = 'Rodrigo';
-// let apellido = 'Aranda';
-// console.log(nombre+' '+apellido); opcion 1
-// console.log(`Nombre y Apellido: ${nombre},${apellido}`); opcion 2 y la mas utilizada
+// const nombreProyecto = 'Kierrez Zer My Nobia';
+
+// nombre2 = 'Leo';
+
+// // console.log(nombre1);
+// console.log(nombre2);
+// // console.log(nombre3);
+
+// nombre1 = 'Elliot';
+// var nombre2 = 'Mario';
+// var nombre2 = 'Jarod';
+// let nombre3 = 'Juan';
+// // let nombre3 = 'José';
+
+// console.log(nombre1);
+// console.log(nombre2);
+// console.log(nombre3);
+// console.log(nombre3);
+
+// console.log(nombre);
+// nombre = 'Elliot';
+// console.log(nombre);
+// var nombre;
+// var nombre = 'Leo';
+// console.log(nombre);
+// nombre = 'Leoncio';
+// console.log(nombre);
+
+// let apellido = 'Garamendi';
+// console.log(apellido);
+// // let apellido = 'Garamendi';
+// apellido = 'Sarmiento';
+// console.log(apellido);
+
+// var comida1;
+// let comida2;
+// // const comida3;
+
+// let nombre = 'Elliot';
+// let apellido = 'Garamendi';
+
+// // console.log('Nombre: ' + nombre + ', Apellido: ' + apellido);
+// console.log(`Nombre: ${nombre}, Apellido: ${apellido}`);
 
 // const edad = 26;
+// // if (edad < 18) {
+// //   console.log('Usted es menor de edad');
+// // }
+
 // if (edad < 18) {
-//     console.log(`Usted es menor de edad`);
+//   console.log('Usted es menor de edad');
 // } else {
-//     console.log(`Usted es mayor de edad`);
+//   console.log('Usted es mayor de edad');
 // }
 
 // const numero1 = 8;
-// const numero2 = 8;
+// const numero2 = 5;
 
-// if(numero1>numero2){
-//     console.log(`El numero1: ${numero1} es mayor`);
-// } else if(numero2>numero1){
-//     console.log(`El numero2: ${numero2} es mayor`)
+// if (numero1 > numero2) {
+//   console.log(`El número 1 (${numero1}) es mayor`);
+// } else if (numero2 > numero1) {
+//   console.log(`El número 2 (${numero2}) es mayor`);
 // } else {
-//     console.log(`El numero1: ${numero1} es igual al numero2: ${numero2}`);
+//   console.log(`Son iguales (${numero1} y ${numero1})`);
 // }
 
 // const tabla = 2;
-// console.log(`INICIO`);
-// for (let i = 1; i <= 12; i++){
-//     console.log(`${tabla} x ${i} = ${tabla * i}`);
+
+// for (let i = 1; i <= 12; i++) {
+//   console.log(`${tabla} x ${i} = ${tabla * i}`);
 // }
-// console.log(`FIN`);
 
 // let i = 1;
 // do {
-//     console.log(`${tabla} x ${i} = ${tabla * i}`);
-//     i++;
-// } while (i <= 12);
+//   console.log(`${tabla} x ${i} = ${tabla * i}`);
+//   i++;
+// } while (i <= -1);
 
-// while (i<=12) {
-//     console.log(`${tabla} x ${i} = ${tabla * i}`);
-//     i++;
+// while (i <= 12) {
+//   console.log(`${tabla} x ${i} = ${tabla * i}`);
+//   i++;
 // }
 
 // const estofados = ['Cuy', 'Conejo', 'Gato', 'Camello'];
 
 // for (let i in estofados) {
-//     console.log(`Animal ${+i + 1}: ${estofados[i]}`);
+//   console.log(`Estofado ${+i + 1}: ${estofados[i]}`);
 // };
 
 // for (let elemento of estofados) {
-    //     console.log(`Estofados: ${elemento}`);
-    // };
-    
-    // const persona = new Object(); no es la mejor opcion para crear objectos
-    // persona.nombre = 'Rodrigo';
-    // console.log(persona);
+//   console.log(`Estofado: ${elemento}`);
+// };
 
-    // const perro1 ={                      la mejor opcion de crear objetos y la mas utilizada
-//     nombre: 'Odin',
-//     raza: 'Chusterman',
-//     color: 'Crema y blanco',
-//     fechaNacimiento: '23/01/2017',
-//     edad: 5,
-//     vacunado: true,
-//     tamaño: 'Grande',
-//     dueño: 'Rodrigo',
-//     ladrar: function () {
-//         return 'Guau Guau';
-//     }
+// const persona1 = new Object();
+// console.log(persona1);
+// persona1.nombre = 'Elliot';
+// console.log(persona1);
+
+// const perro1 = {
+//   nombre: 'Elliot',
+//   edad: 26,
+//   raza: 'Chusca',
+//   color: 'Amarillo',
+//   fechaNacimiento: '31/01/1996',
+//   vacunado: false,
+//   size: 'Grande',
+//   owner: 'La elfa lvl XY',
+//   ladrar: function () {
+//     return 'Guaf guaf';
+//   }
 // };
 
 // console.log(perro1);
 // console.log(perro1.nombre);
 // console.log(perro1['edad']);
 // console.log(perro1.ladrar());
+// console.log(perro1.apellido);
 
-// CLASE
 // function Perro(nombre, edad, raza, color, fechaNacimiento) {
+//   this.nombre = nombre;
+//   this.edad = edad;
+//   this.raza = raza;
+//   this.color = color;
+//   this.fechaNacimiento = fechaNacimiento;
+//   this.ladrar = function () {
+//     return 'Guaf guaf';
+//   };
+// }
+
+// class Perro {
+//   constructor(nombre, edad, raza, color, fechaNacimiento) {
 //     this.nombre = nombre;
 //     this.edad = edad;
 //     this.raza = raza;
 //     this.color = color;
 //     this.fechaNacimiento = fechaNacimiento;
-//     this.ladrar = function(){
-//         return 'GUAU GUAU';
-//     }
+//   }
+
+//   ladrar() {
+//     return 'Guaf Guaf';
+//   }
 // }
 
-// const perro1 = new Perro('Odin', 5, 'Chusterman', 'Crema y blanco', '23/01/2017')
+// const perro1 = new Perro('Elliot', 26, 'Chusca', 'Amarillo');
 // console.log(perro1);
-// const perro2 = new Perro('Panda', 7, 'Chusterman', 'Blanco y marron', '23/01/2017')
+// console.log(perro1.ladrar());
+// const perro2 = new Perro('Leo', 26, 'Chusca', 'Amarillo');
 // console.log(perro2);
-// const perro3 = new Perro('Pardo', 1, 'Chusterman', 'Negro', '23/01/2017')
+// console.log(perro2.ladrar());
+// const perro3 = new Perro('Leoncio', 26, 'Chusca', 'Amarillo');
 // console.log(perro3);
+// console.log(perro3.ladrar());
 
-// Definiendo Clases
+// const numeros = [8, 88, 5, 55, 18];
+// console.log(numeros);
+// console.log(numeros[3]);
 
-// class Perro {
-//     constructor(nombre, edad, raza, color, fechaNacimiento){
-//         this.nombre = nombre;
-//         this.edad = edad;
-//         this.raza = raza;
-//         this.color = color;
-//         this.fechaNacimiento = fechaNacimiento;
-//     }
-    
-//     ladrar(){
-//         return 'GUAU GUAU';
-//     }
-// }
-
-// const perro1 = new Perro('Odin', 5, 'Chusterman', 'Crema y blanco', '23/01/2017')
-// console.log(perro1);
-// const perro2 = new Perro('Panda', 7, 'Chusterman', 'Blanco y marron', '23/01/2017')
-// console.log(perro2);
-// const perro3 = new Perro('Pardo', 1, 'Chusterman', 'Negro', '23/01/2017')
-// console.log(perro3);
-
-// ARRAY
-
-// const nombres = ['Rodrigo', 'Estefany', 'Mathias', 'Antonio', 'Regina']
+// const nombres = ['Elliot', 17, true, '2000/20/12', null, undefined, [1, 2, 3], {perro: 'Elliot'}];
 // console.log(nombres);
-// console.log(nombres[4]);
+// console.log(nombres.length);
 
-// const rodrigo = ['Rodrigo Aranda', 26, '18/02/1996', 'Computacion e informatica', true]
-// console.log(rodrigo);
-
-// const comidas =['Aji de Gallina', 'Tallarines Verdes', 'Arroz a la Cubana', 'chafufa'];
+// 'Estofado de pez', 'Puré, 'Pollo a la brasa', 'Alitas a la BBQ', 'Sancochado de Gato', 'Adobo de Chancho'
+// const comidas = ['Ají de Gallina', 'Aguadito de Pato', 'Chaufa', 'Picante de Gato'];
 // console.log(comidas);
-// console.log(comidas.push('Ceviche'));
+// console.log(comidas.push('Estofado de pez'));
 // console.log(comidas);
-// console.log(comidas.unshift('Pure de Papas'));
+// console.log(comidas.unshift('Puré de Papa'));
 // console.log(comidas);
 // console.log(comidas.pop());
 // console.log(comidas);
@@ -147,3 +261,113 @@
 // console.log(comidas);
 // console.log(comidas.unshift('Pollo a la brasa'));
 // console.log(comidas);
+// console.log(comidas.shift('Pollo a la brasa'));
+// console.log(comidas);
+// console.log(comidas.push('Alitas a la BBQ'));
+// console.log(comidas);
+// console.log(comidas.splice(1, 2));
+// console.log(comidas);
+// console.log(comidas.splice(0, 0, 'Sancochado de Gato', 'Adobo de Chancho'));
+// console.log(comidas);
+
+// const numeros = [2, 5, 8, 7, 10, 1, 8, 3];
+// console.log(numeros.indexOf(10));
+// console.log(numeros.indexOf(11));
+// console.log(numeros.indexOf(8));
+// console.log(numeros.lastIndexOf(8));
+// console.log(numeros.find(element => element > 7));
+// console.log(numeros.findIndex(element => element > 7));
+// console.log(numeros.some(element => element < 2));
+// console.log(numeros.some(element => element < 1));
+// console.log(numeros.every(element => element < 10));
+// console.log(numeros.every(element => element < 11));
+// console.log(numeros.includes(10));
+// console.log(numeros.includes(11));
+
+// console.log(numeros);
+// console.log(numeros.join(', '));
+// console.log(numeros.toString());
+// console.log(numeros.sort());
+// console.log(numeros);
+// console.log(numeros.sort((a, b) => a - b));
+// console.log(numeros);
+// console.log(numeros.reverse());
+// console.log(numeros);
+// console.log(
+//   numeros.forEach(element => {
+//     console.log(element);
+//   })
+// );
+// console.log(numeros);
+// console.log(numeros.map(element => element * 2));
+// console.log(numeros.filter(element => element > 5));
+// console.log(numeros.reduce((valorAnterior, valorActual) => valorAnterior + valorActual, 0));
+// console.log(numeros.reduce((valorAnterior, valorActual) => {
+//   console.log(valorAnterior);
+//   console.log(valorActual);
+//   return (valorAnterior + valorActual);
+// }, 10));
+// console.log(numeros);
+
+// const frutas = ['platano','manzana', 'fresa', 'mandarina'];
+
+// const ensaladas = frutas.map((fruta) => {
+//     return (`Ensalada de ${fruta}`);
+// });
+// console.log(ensaladas);
+
+// function esMayor(edad) {
+//     let respuesta = '';
+//     if( edad >= 18){
+//         respuesta = 'Es mayor';
+//     }else {
+//         respuesta = 'Es menor de edad';
+//     }
+//     return (respuesta);
+// }
+
+// console.log(esMayor(26));
+// console.log(esMayor(20));
+// console.log(esMayor(17));
+
+// const esMayor = function(edad){
+//     return(edad >= 18 ? 'Es mayor' : 'Es menor');
+// };
+
+// const esMayor = edad => edad >= 18 ? 'Es Mayor' : 'Es menor';
+
+// console.log(esMayor(26));
+// console.log(esMayor(20));
+// console.log(esMayor(17));
+
+// console.log(window);
+// comida = 'Aji de Gallina';
+// console.log(window);
+// console.log(window.document);
+// console.log(alert('Te quiero mucho 😘'));
+// console.log(confirm('¿Quisieras estar conmigo?'));
+// console.log(prompt('¿Por qué?'));
+
+// const intervalId = setInterval(() => {
+//     console.log('No quiero estar contigo');
+// }, 1000);
+// console.log(intervalId);
+// console.log(setTimeout(() => {
+//     console.log('Matar rechazo');
+//     clearInterval(intervalId);
+// }, 5000));
+
+// window.onclick = () => {
+//     alert('Quererte pude, olvidarte no');
+// };
+// console.log(window);
+
+console.log(document);
+const title = document.getElementById('title');
+console.log(title);
+const partner = prompt('Dime tu nombre')
+console.log(title.innerText +=  ` ${partner} ?`);
+const mainButton = document.querySelector('button');
+console.log(mainButton);
+const buttons = document.querySelectorAll('button');
+console.log(buttons);
