9087e4353f953611d654ca9a2546f56ce8d980dd
Drocu <drocu99@gmail.com>
2022-09-10 11:25:19 -0500

    s6: se agrego un carrusel de img

--- semana-6/1-estofado-store/js/index.js
+++ semana-6/1-estofado-store/js/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import header from "./modules/header.js";
+import slider from "./modules/slider.js";
 
 const documentReady = () => {
     header();
+    slider();
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
